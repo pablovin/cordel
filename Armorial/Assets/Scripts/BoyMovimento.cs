ef35ccad8c152589d3f29120ad5804185219bef8
Francisco Junior <uzumakijunior@gmail.com>
2016-01-14 16:01:45 -0300

    Animacao Boy sem movimento automatico

---
+++ Armorial/Assets/Scripts/BoyMovimento.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoyMovimento : MonoBehaviour
+{
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		var vertical = Input.GetAxis("Vertical");
+		var horizontal = Input.GetAxis("Horizontal");
+
+//		if (vertical > 0)
+//		{
+//			animator.SetInteger("Direcao", 2);
+//		}
+//		else if (vertical < 0)
+//		{
+//			animator.SetInteger("Direcao", 0);
+//		}
+		if (horizontal < 0)
+		{
+			animator.SetBool("Esquerda", true);
+		}
+		else if (horizontal > 0)
+		{
+			animator.SetBool("Esquerda", false);
+		}
+	}
+}
